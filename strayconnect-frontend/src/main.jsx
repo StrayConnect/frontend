eb457f66498995b7f31ece554add7852d24c71e6
Chitranssh <saxenaayush2052001@gmail.com>
2023-12-14 20:43:04 +0530

    Added Global properties

--- strayconnect-frontend/src/main.jsx
+++ strayconnect-frontend/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <App />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
